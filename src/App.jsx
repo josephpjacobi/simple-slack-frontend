ea421f2132d25428dbcab634246043b2e0d2a1d1
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-03-18 13:35:32 -0400

    added functionality to delete a message

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { NavColumn } from "./nav-column/nav-column";
-import MessageList from "./message-list/message-list";
-import InputBox from "./input/input-box";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { NavColumn } from './nav-column/nav-column';
+import MessageList from './message-list/message-list';
+import InputBox from './input/input-box';
 
 function App() {
-  const [selectedChannel, setSelectedChannel] = useState("");
+  const [selectedChannel, setSelectedChannel] = useState('');
   const [allChannels, setAllChannels] = useState([]);
   const [allPeople, setAllPeople] = useState([]);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     Promise.all([
-      fetch("http://localhost:3001/channels").then(channels => channels.json()),
-      fetch("http://localhost:3001/users").then(users => users.json())
+      fetch('http://localhost:3001/channels').then((channels) => channels.json()),
+      fetch('http://localhost:3001/users').then((users) => users.json())
     ]).then(([channels, users]) => {
       setAllChannels(channels);
       setAllPeople(users);
@@ -21,8 +21,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const buildEndpoint = channel => {
-      if (allPeople.find(user => user.username === channel)) {
+    const buildEndpoint = (channel) => {
+      if (allPeople.find((user) => user.username === channel)) {
         return `http://localhost:3001/messages/username/${channel}`;
       }
       return `http://localhost:3001/messages/channelname/${channel}`;
@@ -31,24 +31,40 @@ function App() {
     if (selectedChannel) {
       const endpoint = buildEndpoint(selectedChannel);
       fetch(endpoint)
-        .then(response => response.json())
-        .then(newMessages => setMessages(newMessages));
+        .then((response) => response.json())
+        .then((newMessages) => setMessages(newMessages));
     }
     return setMessages([]);
   }, [selectedChannel, allPeople]);
 
-  // It is creating a new message in the database but not using the data I provided in the body
-  const addMessage = newMessage => {
-    fetch("http://localhost:3001/messages", {
-      method: "POST",
+  const addMessage = (newMessage) => {
+    fetch('http://localhost:3001/messages', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
-      mode: "cors",
+      mode: 'cors',
       body: JSON.stringify(newMessage)
     })
-      .then(response => response.json())
-      .then(newMessages => setMessages(newMessages));
+      .then((response) => response.json())
+      .then((newMessages) => setMessages(newMessages));
+  };
+
+  const deleteMessage = (message) => {
+    fetch('http://localhost:3001/messages', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      body: JSON.stringify(message)
+    })
+      .then((response) => response.json())
+      .then((allMessages) => {
+        // eslint-disable-next-line no-console
+        console.log(`Message deleted with id ${message.messageid}`);
+        setMessages(allMessages);
+      });
   };
 
   return (
@@ -66,7 +82,7 @@ function App() {
             />
           </nav>
           <main className="content">
-            <MessageList messages={messages} />
+            <MessageList messages={messages} deleteMessage={deleteMessage} />
             <InputBox
               selectedChannel={selectedChannel}
               addNewMessage={addMessage}
